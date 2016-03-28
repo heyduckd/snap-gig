ac1615bf01e5b1312293b36ffda9c988c4dccccc
Hugo A Guerra <guerrahugoa@gmail.com>
2016-03-28 11:15:21 -0700

    initialized gig schema file

--- models/gigs-schema.js
+++ models/gigs-schema.js
@@ -1,1 +1,15 @@
-'use strict'
+'use strict';
+
+let mongoose = require('mongoose');
+
+let gigSchema = new mongoose.Schema ({
+  name: {type: String, required: true},
+  category: String,
+  description: {type: String, required: true},
+  deadline: Date,
+  payment_range: Number,
+  owner: [{type:mongoose.Schema.Types.ObjectId, ref: 'User'}],
+  submissions: [{type:mongoose.Schema.Types.ObjectId, ref: 'Submission'}]
+});
+
+module.exports = mongoose.model('Gig', gigSchema)

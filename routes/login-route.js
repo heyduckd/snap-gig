e4683351ae7dae00eed27097e5bd3f735bfaaa84
YOUR FULL NAME <Ho.luc13@gmail.com>
2016-03-28 11:26:15 -0700

    added login route

--- routes/login-route.js
+++ routes/login-route.js
@@ -3,8 +3,21 @@
 const jwt = require('jsonwebtoken');
 const User = require(__dirname + '/../models/users-schema');
 
-module.exports = (router) => {
-  router.post('/login', (req, res) => {
-    
+module.exports = (loginRouter) => {
+  loginRouter.post('/login', (req, res) => {
+    let based64ed = req.headers.authorization.split(' ')[1];
+    let authArray = new Buffer(based64ed, 'base64').toString().split(':');
+    let name = authArray[0];
+    let password = authArray[1];
+
+    User.find({username: name}, (err, user) => {
+      let valid = user[0].compareHash(password);
+      if(!valid) {
+        return res.status(400).json(message: 'invalid login')
+      }
+
+      res.status(200).json({token: user[0].generateToken()})
+      res.end();
+    })
   })
 }

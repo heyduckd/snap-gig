e22b7fbef1a3b607f4cc8fa0b1bec34ffd3293b7
Hugo A Guerra <guerrahugoa@gmail.com>
2016-03-29 14:59:14 -0700

    fixed user/:id put and delete routes in user-routes, added adminRouter to server.js, modified admin routes to require no permissions

--- routes/user-route.js
+++ routes/user-route.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const User = require(__dirname + '/../models/users-schema');
 const http = require('http');
 const fs = require('fs');
+const auth = require(__dirname + '/../lib/authentication');
 
 module.exports = (publicRouter) => {
   publicRouter.route('/user')
@@ -28,11 +29,11 @@ module.exports = (publicRouter) => {
     });
 
     publicRouter.route('/user/:id')
-    .put((req, res) => {
+    .put(auth, (req, res) => {
       let userInfo = req.user._id;
       req.on('data', (data) => {
         req.body = JSON.parse(data);
-        if (req.params.id === userInfo) {
+        if (req.params.id == userInfo) {
           User.update({_id: req.params.id}, req.body, (err, user) => {
             if (err) {
               res.status(404).json({msg: 'User not found'});
@@ -47,26 +48,33 @@ module.exports = (publicRouter) => {
         }
       });
     })
-    .delete((req, res) => {
-      let userInfo = req.user._id
-      if (req.params.id === userInfo) {
-      User.findById(req.params.id, (err, user) => {
-        if (err) {
-          res.status(404).json({msg: 'User not found'});
-          res.end();
-        }
-        user.remove((err, user) => {
+    .delete(auth, (req, res) => {
+      console.log('delete route hit');
+      console.log('req user id is ', req.user._id);
+      console.log('req params id is ', req.params.id);
+      let userInfo = req.user._id;
+      if (req.params.id == userInfo) {
+        User.findById(req.params.id, (err, user) => {
+          console.log('user is: ' + user);
+          console.log('error is: ' + err);
           if (err) {
-            res.status(404).json({msg: 'User coundn\'t be deleted'});
+            res.status(404).json({msg: 'User not found'});
             res.end();
           }
-          res.status(200);
-          res.json({msg: 'User ' + req.params.id + ' has been deleted.'});
-          res.end();
+          user.remove((err, user) => {
+            if (err) {
+              res.status(404).json({msg: 'User coundn\'t be deleted'});
+              res.end();
+            }
+            res.status(200);
+            res.json({msg: 'User ' + req.params.id + ' has been deleted.'});
+            res.end();
+          });
         });
-      });
-    } else {
-      res.status(404).json({msg: 'You do not have permissions to delete this user!'});
-    }
+      } else {
+        console.log('in else no permission');
+        res.status(404).json({msg: 'You do not have permissions to delete this user!'});
+        res.end();
+      }
     });
-  };
+};

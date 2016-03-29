c536fd23442c45f7f8de66c5b4747e187f2706c9
YOUR FULL NAME <Ho.luc13@gmail.com>
2016-03-29 15:15:41 -0700

    added edits to gig route and sub-schema and readme

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # snap-gig
 A web app where users can post freelance gigs. Users who are not gig managers are then able to submit files to specific gigs. Submissions are judged by the gig manager and winning submissions are selected, notifying the submission owner.
+
+# dev quick curl commands
+make user:
+curl -X POST --data '{"username":"testname2", "password":"123456", "email":"poop2@gmail.com", "occupation":"web-design"}' localhost:3000/public/user
+
+login with user above: (gets you a token)
+curl -X POST -u testname2:123456 localhost:3000/login/login
+
+GET to /api/gigs: (to show all gigs)
+curl -X GET -H 'Authorization: token INSERT YOUR_TOKEN_HERE' localhost:3000/api/gigs
+
+POST to /api/gigs: (to post a gig)
+curl -X POST -H 'Authorization: token INSERT YOUR_TOKEN_HERE' --data '{"name":"logo", "category":"pics", "description":"need a logo for my site", "deadline": "1-26-2016", "payment_range":500}' localhost:3000/api/gigs
+
+POST to /api/gigs/:id/submissions (to post a submission to a specific id)
+curl -X POST -H 'Authorization: token INSERT YOUR_TOKEN_HERE' --data '{"name":"logo", "category":"pics", "description":"need a logo for my site", "deadline": "1-26-2016", "payment_range":500}' localhost:3000/api/gigs/:SPECIFIC_ID/submissions
--- routes/gigs-route.js
+++ routes/gigs-route.js
@@ -2,103 +2,125 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const http = require('http');
 const fs = require('fs');
 const jwtAuth = require(__dirname + '/../lib/authentication');
-
 const Gig = require(__dirname + '/../models/gigs-schema');
 const Sub = require(__dirname + '/../models/submissions-schema');
 const User = require(__dirname + '/../models/users-schema');
+const AWS = require('aws-sdk');
+let s3 = new AWS.S3();
 
 module.exports = (apiRouter) => {
   apiRouter.route('/gigs')
-  .get((req, res) => {
-    Gig.find({}).populate('owner').populate('submissions').exec((err, gigs) => {
-      if(err) throw err
-      res.status(200).json(gigs)
-      res.end()
-    })
-  })
-  .post((req, res) => {
-    req.on('data', (data) => {
-      let userInfo = req.user._id;
-      req.body = JSON.parse(data)
-      var newGig = new Gig(req.body)
-      newGig.save((err, gig) => {
-        if(err) throw err;
-        User.findByIdAndUpdate(userInfo, { $push: {gigs: gig._id}}, (err, user) => {
-        });
-        res.status(200);
+    .get((req, res) => {
+      Gig.find({}).populate('owner').populate('submissions').exec((err, gigs) => {
+        if(err) throw err
+        res.status(200).json(gigs)
         res.end()
       })
     })
+    .post((req, res) => {
+      req.on('data', (data) => {
+        let userInfo = req.user._id;
+        req.body = JSON.parse(data)
+        var newGig = new Gig(req.body)
+        newGig.save((err, gig) => {
+          if(err) throw err;
+          User.findByIdAndUpdate(userInfo, { $push: {gigs: gig._id}}, (err, user) => {
+          });
+          res.status(200).json({data: gig});
+          res.end()
+        })
+      })
   })
 
   apiRouter.route('/gigs/:id')
-  .get((req, res) => {
-    Gig.findById(req.params.id, (err, gig) => {
-      if (err) throw err
-      res.json(gig)
-      res.status(200)
-      res.end()
-    })
-  })
-  .patch((req, res) => {
-  let userInfo = req.user._id;
-  Gig.findById(req.params.id, (err, gig) => {
-    if (err) throw err;
-    if (gig.owner === userInfo) {
-      gig.update(req.body, (err, data) => {
-        if (err) throw err;
+    .get((req, res) => {
+      Gig.findById(req.params.id, (err, gig) => {
+        if (err) {
+          res.status(400).json({msg: 'Couldn\'t find gig'});
+        }
+        res.status(200).json({data: gig});
+        res.end()
       })
-    } else {
-      res.status(404).json({msg: 'You do not have permissions to patch this gig!'});
-    };
-  });
-})
-  .delete((req, res) => {
-    let userInfo = req.user._id;
-    Gig.findById(req.params.id, (err, gig) => {
-      if (err) throw err;
-      if (gig.owner === userInfo) {
-        gig.remove((err, gig) => {
-          res.json({msg: 'gig removed!!'})
-        });
-      } else {
-        res.json({msg: 'you dont have permission to delete this user'});
-      };
-    });
+    })
+    .patch((req, res) => {
+      let userInfo = req.user._id;
+      Gig.findById(req.params.id, (err, gig) => {
+        if (err) throw err;
+        if (gig.owner === userInfo) {
+          gig.update(req.body, (err, data) => {
+            if (err) throw err;
+          })
+        } else {
+          res.status(404).json({msg: 'You do not have permissions to patch this gig!'});
+        };
+      });
+    })
+    .delete((req, res) => {
+      let userInfo = req.user._id;
+      Gig.findById(req.params.id, (err, gig) => {
+        if (err) throw err;
+        if (gig.owner === userInfo) {
+          gig.remove((err, gig) => {
+            res.json({msg: 'gig removed!!'})
+          });
+        } else {
+          res.json({msg: 'you dont have permission to delete this user'});
+        };
+      });
   });
 
   apiRouter.route('/gigs/:id/submissions')
-  .post((req, res) => {
-    req.on('data', (data) => {
-      req.body = JSON.parse(data);
-      let newSub = new Sub(req.body);
-      console.log('REQEST ID FOR SUBMISSION USER : ', req.user._id);
-      console.log(req.body);
-      newSub.save((err, submission) => {
-        if (err) {
-          res.status(404).json({msg: 'Invalid Submission'});
-          res.end();
+    .post((req, res) => {
+      req.on('data', (data) => {
+        var newBody;
+        req.body = JSON.parse(data);
+        let newSub = new Sub(req.body);
+        let body = fs.readFile(__dirname + '/../db/testingfile.txt', (err, readFile) => {
+          if (err) {
+            res.status(400).json({msg: 'AWS error: ' + err});
+          }
+          var newBody = JSON.parse(readFile);
+        })
+        console.log('NEW BODY : ', newBody)
+
+        let params = {
+          Bucket: 'snap-gig-submit-bucket',
+          Key: req.body.name,
+          ACL: 'public-read-write',
+          Body: newBody
         }
-        let submissionId = submission._id;
-        Gig.findByIdAndUpdate(req.params.id, {$push: {submissions: submissionId}}, (err, subId) => {
+
+        newSub.save((err, submission) => {
           if (err) {
             res.status(404).json({msg: 'Invalid Submission'});
             res.end();
           }
+          console.log('REQUEST PARAMS ID : ' + submission);
+          let submissionId = submission._id;
+          Gig.findByIdAndUpdate(req.params.id, {$push: {submissions: submissionId}}, (err, subId) => {
+            if (err) {
+              res.status(404).json({msg: 'Invalid Submission'});
+              res.end();
+            }
+          })
+          User.findByIdAndUpdate(req.user._id, {$push: {submissions: submissionId}}, (err, subId) => {
+            if (err) {
+              res.status(404).json({msg: 'Invalid Submission'});
+              res.end();
+            }
+          })
+          s3.putObject(params, (err, data) => {
+            if (err) {
+              res.status(400).json({msg: 'Can\'t save file due to err: ' + err});
+            }
+            // console.log('this is data: ', data);
+          })
+          res.status(200).json(submission);
+          res.end();
+          // Still need to implement S3 save and grab of saved URL. Also grabbing "CHUNKS" of attachment data
         })
-        User.findByIdAndUpdate(req.user._id, {$push: {submissions: submissionId}}, (err, subId) => {
-          if (err) {
-            res.status(404).json({msg: 'Invalid Submission'});
-            res.end();
-          }
-        })
-        res.status(200).json(submission);
-        res.end();
-        // Still need to implement S3 save and grab of saved URL. Also grabbing "CHUNKS" of attachment data
       })
-    })
   })
 }

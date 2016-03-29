ae25b3f6c16ba890c8710c2f57fa112d94d89174
Sam Gruse <sgruse89@gmail.com>
2016-03-29 16:57:47 -0700

    Finally got S3 to take in a hardcoded document

--- models/submissions-schema.js
+++ models/submissions-schema.js
@@ -4,8 +4,10 @@ let mongoose = require('mongoose');
 
 let submissionSchema = new mongoose.Schema ({
   owner: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
-  name: {type: String, required: true, unique: true},
-  body: {type: String, required: true},
+  name: {type: String},
+  body: {type: String},
+  // name: {type: String, required: true, unique: true},
+  // body: {type: String, required: true},
   files: String,
   updated: {type: Date, default: Date.now}
 });
